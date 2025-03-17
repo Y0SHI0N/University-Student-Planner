aff504cd47964094a72c0d2e3bb85733d6631daa
J-VonDohren <Pastelanemone@gmail.com>
2025-03-17 14:33:58 +1000

    updated fxml and controllers split up

--- src/main/java/Application/DatabaseConnection.java
+++ src/main/java/Application/DatabaseConnection.java
@@ -1,23 +1,55 @@
 package Application;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
-    public Connection databaseLink;
+    private static DatabaseConnection instance;
+    private static HikariDataSource dataSource;
 
-    public Connection getConnection(){
-        String databaseName = "student_planner_db";
-        String databaseUser = "student_user";
-        String databasePassword = "SecurePass123";
-        String url = "jdbc:mysql://localhost:3306/" + databaseName + "?serverTimezone=UTC";
+    private DatabaseConnection() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/student_planner_db?serverTimezone=UTC");
+        config.setUsername("student_user");
+        config.setPassword("SecurePass123");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30000);
+        config.setMaxLifetime(1800000);
 
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
-        } catch (Exception e){
-            System.out.println(e + "exception");
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
         }
-        return databaseLink;
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closeResources(Statement statement, ResultSet resultSet) {
+        try {
+            if (resultSet != null) resultSet.close();
+            if (statement != null) statement.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -1,29 +1,83 @@
 package Application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
-import java.util.Objects;
+import java.io.IOException;
+import java.sql.Connection;
 
 public class Main extends Application {
+
+    private Parent root;
+    private Stage applicationStage = new Stage(); //global stage reference
+    protected FXMLLoader homePage;
+    protected FXMLLoader loginPage;
+    protected FXMLLoader registerPage;
+    protected FXMLLoader goalsPage;
+    protected FXMLLoader mapPage;
+    protected FXMLLoader calendarPage;
+
+    public void changeScene(String fxmlFilePath) throws IOException{
+        try{
+            FXMLLoader fxmlFile = new FXMLLoader(getClass().getResource(fxmlFilePath));
+            root = fxmlFile.load();
+
+            formatStage();
+
+            applicationStage.setScene(new Scene(root));
+            applicationStage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
     public static void main(String[] args) {
 
         launch(args);
     }
 
     @Override
-    public void start(Stage LogInStage) throws Exception {
-        Parent LoginRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Login-Page.fxml")));
-        LogInStage.initStyle(StageStyle.UNDECORATED);
-        LogInStage.setScene(new Scene(LoginRoot));
-        LogInStage.getIcons().add(new Image("Img/QUT-Logo.jpg"));
-        LogInStage.show();
+    public void start(Stage applicationStage) throws Exception {
+        this.applicationStage = applicationStage; // syncs the 2 stages
+
+        loadAllFXMLFiles(); // stores all fxml files in a FXMLLoader protected variable for use in the sceneLoaderController
+
+        changeScene("/Login-Page.fxml"); // loads the login page as the first scene
+
+        applicationStage.show(); // shows the stage
+    }
+    public void formatStage(){
+        applicationStage.getIcons().add(new Image("Img/QUT-Logo.jpg"));
+    }
+    public void closeActiveStage(){
+        Platform.runLater(() -> {
+            for (Window window : Stage.getWindows()) {
+                if (window instanceof Stage) { // doesn't close every window open
+                    ((Stage) window).close();
+                }
+            }
+        });
+    }
+
+    public void loadAllFXMLFiles(){
+        try{
+        loginPage = new FXMLLoader(getClass().getResource("/Login-Page.fxml"));
+        registerPage = new FXMLLoader(getClass().getResource("/Register-Page.fxml"));
+        homePage = new FXMLLoader(getClass().getResource("/Home-Page.fxml"));
+        goalsPage = new FXMLLoader(getClass().getResource("/Goals-Page.fxml"));
+        mapPage = new FXMLLoader(getClass().getResource("/Map-Page.fxml"));
+        calendarPage = new FXMLLoader(getClass().getResource("/Calendar-Page.fxml"));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }

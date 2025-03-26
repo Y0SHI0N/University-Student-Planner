ec4cc0f918c38440c52ab26cfe8adaaa219c213c
J-VonDohren <Pastelanemone@gmail.com>
2025-03-27 08:59:53 +1000

    added an AI_model java class that is responsible for managing the connection between the locally run ai model and the student planner application

---
+++ src/main/java/Application/AI_model.java
@@ -0,0 +1,58 @@
+package Application;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class AI_model {
+    public String modelName = "gemma2:2b";
+    public static String reponseText;
+    public HttpURLConnection conn;
+
+    public void initialiseAIModel(){
+        try{
+        URL url = new URL("http://localhost:11434/api/generate");
+        conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setDoOutput(true);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void promptAI(String promptText)
+    {
+        try{
+            String formattedString = "{\"model\": \"%s\", \"prompt\": \"%s\", \"stream\": false}";
+            String jsonInputString = String.format(formattedString, modelName, promptText);
+
+            try(OutputStream os = conn.getOutputStream()){
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+            int code = conn.getResponseCode(); // sets the connection state to a code
+            System.out.println(code); // prints the connections state to the console for debugging
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while((line = in.readLine()) != null){
+                response.append(line);
+            }
+            in.close();
+
+            JSONObject jsonResponse = new JSONObject(response.toString());
+            reponseText = jsonResponse.getString("response");
+            System.out.println(reponseText); // prints the response in json format
+            conn.disconnect();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module Application {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.json;
+    requires java.desktop;
 
     // Open package to JavaFX runtime
     opens Application.Controllers to javafx.graphics, javafx.fxml;

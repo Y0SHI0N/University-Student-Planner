cbcd06222ea29007b210593302e16a72071df5d8
J-VonDohren <Pastelanemone@gmail.com>
2025-04-07 14:39:50 +1000

    created the skeleton for the methods needed for full functionality

--- src/main/java/Application/Controllers/goalsPageController.java
+++ src/main/java/Application/Controllers/goalsPageController.java
@@ -1,10 +1,34 @@
 package Application.Controllers;
 
 import Application.Database.DatabaseConnection;
+import Application.Database.UserCollectedData;
 import javafx.fxml.FXML;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class goalsPageController extends sceneLoaderController {
+    protected UserCollectedData userCollectedData;
 
+    public void initialize(){
+
+    }
+
+    public void hideGoals(){
+
+    }
+    public void hideData(){
+
+    }
+    public void updateData(){
+
+    }
+    public void editGoals(){
+
+    }
+    public void displayDataChangesOverTime(){
+
+    }
+    public void generateAIRecommendedGoals(){
+
+    }
 }

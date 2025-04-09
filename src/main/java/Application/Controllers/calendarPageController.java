1b709b4efe0a90f24d0ffc16ecce7b3f1bf3e738
Ben <n12023906@qut.edu.au>
2025-04-09 11:53:27 +1000

    changed controllers to use UserTimetableDAO

--- src/main/java/Application/Controllers/calendarPageController.java
+++ src/main/java/Application/Controllers/calendarPageController.java
@@ -3,7 +3,11 @@ package Application.Controllers;
 import Application.Database.DatabaseConnection;
 import javafx.fxml.FXML;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class calendarPageController extends sceneLoaderController {
+
+
 }
--- src/main/java/Application/Database/UserTimetableDAO.java
+++ src/main/java/Application/Database/UserTimetableDAO.java
@@ -1,4 +1,25 @@
 package Application.Database;
 
-public class UserTimetableDAO {
+import java.sql.*;
+
+public class UserTimetableDAO extends BaseDAO {
+
+    public UserTimetableDAO() {
+        createTable();
+    }
+
+    private void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS User_Timetable_Data (" +
+                "StudentNumber VARCHAR NOT NULL, " +
+                "EventType VARCHAR NOT NULL, " +
+                "EventStartDatetime DATETIME NOT NULL, "+
+                "EventEndDatetime DATETIME NOT NULL, "+
+                "EventLocation VARCHAR, "+
+                "Event_Attendance INT)";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }

2818a0ff0ad3bb275e05d1e5f0918185b2147f91
Jake Von Dohren <Pastelanemone@gmail.com>
2025-04-30 18:04:08 +1000

    Update UserCollectedData.java
    added setters and getters for encapsulation purposes

--- src/main/java/Application/Database/UserCollectedData.java
+++ src/main/java/Application/Database/UserCollectedData.java
@@ -29,21 +29,26 @@ public class UserCollectedData {
         this.unitsEnrolled = unitsEnrolled;
     }
 
-    public String getStudentNumber() { return studentNumber; }
-
-    public String getDateModified() { return dateModified; }
+   public String getStudentNumber() { return studentNumber; }
+    public void setStudentNumber(String studentNumber) { this.studentNumber = studentNumber; }
 
     public int getGpa() { return gpa; }
+    public void setGpa(int gpa) { this.gpa = gpa; }
 
     public int getGpaGoal() { return gpaGoal; }
+    public void setGpaGoal(int gpaGoal) { this.gpaGoal = gpaGoal; }
 
-    public float getHoursStudied() { return hoursStudied; }
+    public String getHoursStudied() { return hoursStudied; }
+    public void setHoursStudied(String hoursStudied) { this.hoursStudied = hoursStudied; }
 
-    public float getHoursStudiedGoal() { return hoursStudiedGoal; }
+    public String getHoursStudiedGoal() { return hoursStudiedGoal; }
+    public void setHoursStudiedGoal(String hoursStudiedGoal) { this.hoursStudiedGoal = hoursStudiedGoal; }
 
-    public float getAttendanceRate() { return attendanceRate * 100; }
+    public float getAttendanceRate() { return attendanceRate; }
+    public void setAttendanceRate(float attendanceRate) { this.attendanceRate = attendanceRate; }
 
-    public float getAttendanceRateGoal() { return attendanceRateGoal * 100; }
+    public float getAttendanceRateGoal() { return attendanceRateGoal; }
+    public void setAttendanceRateGoal(float attendanceRateGoal) { this.attendanceRateGoal = attendanceRateGoal; }
 
     public String getUnitsEnrolled() { return unitsEnrolled; }
     public void setUnitsEnrolled(String unitsEnrolled) { this.unitsEnrolled = unitsEnrolled; }

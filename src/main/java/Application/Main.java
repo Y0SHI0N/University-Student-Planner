cf469794d49190831fd14d0341631ad1fcaf844f
J-VonDohren <pastelanemone@gmail.com>
2025-05-08 10:03:46 +1000

    massive refactor removing Main class dependancies.

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -17,69 +17,20 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.stream.Stream;
 
+import Application.Controllers.sceneLoaderController;
+
 public class Main extends Application {
-    private Parent root;
-    private Stage applicationStage = new Stage(); //global stage reference
     protected FXMLLoader homePage;
     protected FXMLLoader loginPage;
     protected FXMLLoader registerPage;
     protected FXMLLoader goalsPage;
     protected FXMLLoader mapPage;
     protected FXMLLoader calendarPage;
-    protected static String currentUserNumber; //used to track the user currently logged in
-
-    public void changeScene(String fxmlFilePath) throws IOException {
-        try{
-            FXMLLoader fxmlFile = new FXMLLoader(getClass().getResource(fxmlFilePath));
-            root = fxmlFile.load();
-
-            formatStage();
-
-            applicationStage.setScene(new Scene(root));
-            applicationStage.show();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+    protected sceneLoaderController sceneLoaderController = new sceneLoaderController();
+    protected StageController stageController = new StageController();
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage applicationStage) throws Exception {
-        this.applicationStage = applicationStage; // syncs the 2 stages
-
-        loadAllFXMLFiles(); // stores all fxml files in a FXMLLoader protected variable for use in the sceneLoaderController
-
-        changeScene("/FXML/Login-Page.fxml"); // loads the login page as the first scene
-
-        applicationStage.show(); // shows the stage
-    }
-
-    public void formatStage(){
-        try {
-            String imagePath = "/Img/QUT-Logo.jpg";
-            URL imageUrl = getClass().getResource(imagePath);
-
-            if (imageUrl == null) {
-                throw new IllegalArgumentException("Image file not found: " + imagePath);
-            }
-
-            applicationStage.getIcons().add(new Image(imageUrl.toExternalForm()));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error loading stage icon.");
-        }
-    }
-    public void closeActiveStage(){
-        Platform.runLater(() -> {
-            for (Window window : Stage.getWindows()) {
-                if (window instanceof Stage) {
-                    ((Stage) window).close();
-                }
-            }
-        });
     }
 
     public void loadAllFXMLFiles(){
@@ -95,6 +46,11 @@ public class Main extends Application {
             System.out.println(e);
         }
 
+    }
+    @Override
+    public void start(Stage applicationStage) throws Exception {
+        loadAllFXMLFiles(); // stores all fxml files in a FXMLLoader protected variable for use in the sceneLoaderController
+        sceneLoaderController.changeScene("/FXML/Login-Page.fxml"); // loads the login page as the first scene
     }
 
     @Override

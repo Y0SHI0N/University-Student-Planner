60f8c428695875d23efcb0f270d7eb1363723eb9
J-VonDohren <pastelanemone@gmail.com>
2025-05-08 10:04:10 +1000

    massive refactor removing Main class dependancies.

---
+++ src/main/java/Application/StageController.java
@@ -0,0 +1,37 @@
+package Application;
+
+import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.net.URL;
+
+public class StageController {
+    public Stage applicationStage = new Stage();
+
+    public void formatStage(){
+        try {
+            String imagePath = "/Img/QUT-Logo.jpg";
+            URL imageUrl = StageController.class.getResource(imagePath);
+
+            if (imageUrl == null) {
+                throw new IllegalArgumentException("Image file not found: " + imagePath);
+            }
+
+            applicationStage.getIcons().add(new Image(imageUrl.toExternalForm()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error loading stage icon.");
+        }
+    }
+    public static void closeActiveStage(){
+        Platform.runLater(() -> {
+            for (Window window : Stage.getWindows()) {
+                if (window instanceof Stage) {
+                    ((Stage) window).close();
+                }
+            }
+        });
+    }
+}
